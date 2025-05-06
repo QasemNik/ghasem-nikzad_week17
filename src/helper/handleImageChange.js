001dcd8a5ae608a44d6be8243d34ecfbb473e5e1
Ghasem Nikzad <ghasem295@gmail.com>
2025-05-06 15:04:34 +0330

    feat:

--- src/helper/handleImageChange.js
+++ src/helper/handleImageChange.js
@@ -1,16 +1,16 @@
  export function handleImageChange  ( setField)  {
 
-     return (event) => {
-         const file = event.target.files[0];
+    return (event) => {
+        const file = event.target.files[0];
         
-         if (file && file.size <= 5000000) {
-           const reader = new FileReader();
-           reader.onloadend = () => {
-             setField('image', reader.result);
-           };
-           reader.readAsDataURL(file);
-         } else if (file) {
-           alert('Image size should be less than 5MB');
-         }
+        if (file && file.size <= 5000000) {  // 5 Mb
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            setField('image', reader.result);
+          };
+          reader.readAsDataURL(file);
+        } else if (file) {
+          alert('Image size should be less than 5MB');
+        }
     }
   };
